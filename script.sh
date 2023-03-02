987c8a3a219309bbf93218f35aee4c87b321b4bb
adamhrncir <adam@adamhrncir.cz>
2023-03-02 19:02:46 +0100

    script

--- script.sh
+++ script.sh
@@ -1,1 +1,7 @@
-#!bin/sh
+#!bin/bash
+export PBS_REPOSITORY="0.0.0.0:datastore"
+# export PBS_PASSWORD="password"
+# export PBS_PASSWORD_FILE="/path/to/file.txt"
+
+
+proxmox-backup-client backup backup.pxar:/path/to/backup/
