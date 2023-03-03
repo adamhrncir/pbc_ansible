c22ed7f5ba2809284ad202bb370f3058a935f571
daromak <adam@adamhrncir.cz>
2023-03-03 15:04:22 +0100

    created roles and implemented variables into pbc

--- script.sh
+++
@@ -1,7 +0,0 @@
-#!bin/bash
-export PBS_REPOSITORY="0.0.0.0:datastore"
-# export PBS_PASSWORD="password"
-# export PBS_PASSWORD_FILE="/path/to/file.txt"
-
-
-proxmox-backup-client backup backup.pxar:/path/to/backup/
